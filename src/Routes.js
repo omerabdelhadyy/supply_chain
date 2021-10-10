09e120f895c0e88b9f535d45f4982dab19026a99
ome abdelhady <omerabdelhady62@gmail.com>
2021-10-10 17:20:05 +0200

    add new screen (my product && create product)

--- src/Routes.js
+++ src/Routes.js
@@ -2,12 +2,18 @@ import React, { Component } from "react";
 import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 import { register } from "./Pages/register/register";
 import { login } from "./Pages/login/login";
+import { home } from "./Pages/home/home";
+import { CreateProduct } from "./Pages/supplier/createProduct/createProduct";
+import { MyProduct } from "./Pages/supplier/myProducts/myProducts";
 
 class Routes extends Component {
   render() {
     return (
       <BrowserRouter>
         <Switch>
+          <Route path={"/MyProduct"} component={MyProduct} />
+          <Route path={"/Create"} component={CreateProduct} />
+          <Route path={"/home"} component={home} />
           <Route path={"/login"} component={login} />
           <Route path={"/"} component={register} />
         </Switch>
